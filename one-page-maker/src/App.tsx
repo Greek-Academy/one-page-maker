0365f69be4e35a27a4c97e7a696fa592bc0dddf4
bin matsui <BinMatsui@hotmail.com>
2024-02-21 08:50:57 +0900

    env

--- one-page-maker/src/App.tsx
+++ one-page-maker/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <p>test {import.meta.env.VITE_SOME_KEY}</p>    
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- one-page-maker/src/main.tsx
+++ one-page-maker/src/main.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.apiKey,
+//   authDomain: import.meta.env.authDomain,
+//   projectId: import.meta.env.projectId,
+//   storageBucket: import.meta.env.storageBucket,
+//   messagingSenderId: import.meta.env.messagingSenderId,
+//   appId: import.meta.env.appId,
+//   measurementId: import.meta.env.measurementId
+// };
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
