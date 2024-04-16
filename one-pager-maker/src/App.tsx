c4861264f8a1231af229e34c9535bc30d0cdc9ef
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-04-16 22:52:49 +0900

    Add Link

--- one-pager-maker/src/App.tsx
+++ one-pager-maker/src/App.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { Router } from "./Router";
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Top</h1>
-      <nav>
-        <ul>
-          <a href="/Edit">Edit</a>
-          {/* <li><Link to="/">ホーム</Link></li>
-          <li><Link to="/Edit">編集</Link></li>
-          <li><Link to="/List">一覧</Link></li>
-          <li><Link to="/Login">ログイン</Link></li> */}
-        </ul>
-      </nav>      
       <BrowserRouter>
+        <nav>
+          <ul id="nav">
+            <li><Link to="/">ホーム</Link></li>
+            <li><Link to="/edit">編集</Link></li>
+            <li><Link to="/list">一覧</Link></li>
+            <li><Link to="/login">ログイン</Link></li>
+          </ul>
+        </nav>   
         <Router/>
       </BrowserRouter>
     </>
--- one-pager-maker/src/Router.tsx
+++ one-pager-maker/src/Router.tsx
@@ -6,17 +6,9 @@ import Login from "./pages/Login";
 export const Router = () => {
     return (
         <Routes>
-            <Route path="/Edit" element={<Edit/>}/>
-            <Route path="/List" element={<List/>}/>
-            <Route path="/Login" element={<Login/>}/>
-
-            {/* <Route path="/page1" element={<Layout/>}>
-                <Route index element={<Page1/>}/>
-                <Route path="child" element={<Page1Child/>}/>
-            </Route>
-            <Route path="/page2" element={<Page2/>}/> */}
-
-            {/* <Route path="*" element={<Page404/>}/> */}
+            <Route path="/edit" element={<Edit/>}/>
+            <Route path="/list" element={<List/>}/>
+            <Route path="/login" element={<Login/>}/>
         </Routes>
     );
 }
