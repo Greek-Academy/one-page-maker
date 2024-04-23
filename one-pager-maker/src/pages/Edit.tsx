bd78d969cd02c54be564e56b0a73ee72ec3ef3f4
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-04-23 23:45:24 +0900

    List -> Edit

--- one-pager-maker/src/pages/Edit.tsx
+++ one-pager-maker/src/pages/Edit.tsx
@@ -2,62 +2,94 @@ import { useEffect, useState } from 'react'
 import Markdown from 'react-markdown'
 import './Edit.css'
 import { useUpdateDocumentMutation, useFetchDocumentQuery } from "../redux/document/documentsApi.ts";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Document } from "../redux/document/documentType.ts";
 
 function Edit() {
-  const [markdown, setMarkdown] = useState('');
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
   const [document, setDocument] = useState<Document | undefined>(undefined);
-  const toMarkdownText = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMarkdown(e.target.value);
-  };
 
   const urlParams = useParams<{ id: string }>()
+  const navigate = useNavigate();
+  const {data} = useFetchDocumentQuery({uid: 'testUser', docId: urlParams.id ?? ""});
   const [updateDocument] = useUpdateDocumentMutation();
-  const {data, error, isError, isLoading} = useFetchDocumentQuery({uid: 'testUser', docId: urlParams.id})
+  
   useEffect(() => {
     if (data !== undefined) {
       setDocument(data);
+      setTitle(data.title);
+      setContents(data.contents);
     }
   }, [data]);
+
+  const onChangeTitle = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const onChangeContents = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContents(e.target.value);
+  };
+
+  const handleCreate = async () => {
+    try {
+        const result = await updateDocument({
+            uid: 'testUser',
+            documentData: {
+                id:urlParams?.id ?? "",
+                title: title,
+                contents: contents,
+                status: 'draft',
+                owner_id: 'testUser',
+                contributors: [],
+                reviewers: [],
+                url_privilege: 'private',
+            }
+        });
+        if ('data' in result){
+            navigate(`/list`);
+        }
+    } catch (e) {
+        alert(`エラー: ${e?.toString()}`)
+    }
+  }
 
   return (
     <>
     <div>
       <div className="document-title-div">
-        <input className="document-title" type="text"></input>
+        <input className="document-title" type="text" value={title} onChange={onChangeTitle} ></input>
         <button type="button" onClick={() => handleCreate()}>Save</button>
       </div>
       <div className="document-parameter-div">
         <span>
-            <select name="status">
+            <select name="status" value={document?.status}>
               <option value="draft">draft</option>
               <option value="reviewed">reviewed</option>
               <option value="final">final</option>
               <option value="obsolete">obsolete</option>
             </select>
             <span>
-              <input className="authors" type="text"></input>
+              <input className="authors" type="text" value={document?.contributors.join(",")}></input>
             </span>
             <span>
-              <input className="reviewers" type="text"></input>
+              <input className="reviewers" type="text" value={document?.reviewers.join(",")}></input>
             </span>
         </span>
         <span>
             <span className="updated">
-              Updated 2024/01/01
+              Updated {document?.updated_at.toDate().toLocaleString()}
             </span>
         </span>
       </div>
       <div className="document-contents-div">
         <textarea
           className="document-contents"
-          value={markdown}
-          onChange={toMarkdownText}
+          value={contents}
+          onChange={onChangeContents}
           placeholder="Enter Markdown here"
         />
         <div className="document-markdown">
-          <Markdown>{markdown}</Markdown>
+          <Markdown>{contents}</Markdown>
         </div>
       </div>
     </div>
