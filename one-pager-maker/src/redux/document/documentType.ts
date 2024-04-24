633c13509e5e40ba07d0092251dc462984d4bf1c
Harineko0 <harineko0927@gmail.com>
2024-04-24 22:51:55 +0900

    remove as Status[] statement

--- one-pager-maker/src/redux/document/documentType.ts
+++ one-pager-maker/src/redux/document/documentType.ts
@@ -27,7 +27,7 @@ export type DocumentForUpdate = Partial<Omit<Document, "updated_at">> & {
     id: string
 }
 export type Status = z.infer<typeof statusSchema>;
-export const statusValues = Object.values(statusSchema.enum) as Status[];
+export const statusValues = Object.values(statusSchema.enum);
 export type UrlPrivilege = z.infer<typeof privilegeSchema>;
 
 export const documentConverter: FirestoreDataConverter<Document> = {
