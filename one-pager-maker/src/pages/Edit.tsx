ca0859c9f58781c5e73df3ee7c620df6c4e9f038
binnmti <binnmtiisprogrammer@gmail.com>
2024-04-26 21:46:27 +0900

    Review Refactor

--- one-pager-maker/src/pages/Edit.tsx
+++ one-pager-maker/src/pages/Edit.tsx
@@ -1,99 +1,79 @@
 import './Edit.css'
 import { useEffect, useState } from 'react'
+import { useAppSelector } from '../redux/hooks.ts'
 import Markdown from 'react-markdown'
 import { useUpdateDocumentMutation, useFetchDocumentQuery } from "../redux/document/documentsApi.ts";
 import { useNavigate, useParams } from "react-router-dom";
 import { Document, Status } from "../redux/document/documentType.ts";
-import { auth } from '../firebase';
 
 function Edit() {
-  const uid = auth.currentUser?.uid ?? "";
-
-  const [title, setTitle] = useState('');
-  const [contents, setContents] = useState('');
-  const [status, setStatus] = useState<Status>('draft');
-  const [contributors, setContributors] = useState('');
-  const [reviewers, setReviewers] = useState('');
-  const [document, setDocument] = useState<Document | undefined>(undefined);
+  const uid = useAppSelector(state => state.user.user?.uid)
+  const [documentData, setDocumentData] = useState<Document | undefined>(undefined);
   const urlParams = useParams<{ id: string }>()
   const navigate = useNavigate();
-  const {data} = useFetchDocumentQuery({uid: uid, docId: urlParams.id ?? ""});
+  const { data } = useFetchDocumentQuery({ uid: uid ?? "", docId: urlParams.id ?? "" });
   const [updateDocument] = useUpdateDocumentMutation();
-  
-  useEffect(() => {
-    console.log(data);
-    if (data === undefined) return;
-    setDocument(data);
-    setTitle(data.title);
-    setContents(data.contents);
-    setStatus(data.status);
-    setContributors(data.contributors.join(','));
-    setReviewers(data.reviewers.join(','));
-  }, [data]);
 
-  const onChangeTitle = (e:React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
-  const onChangeContents = (e:React.ChangeEvent<HTMLTextAreaElement>) => setContents(e.target.value);
-  const onChangeStatus = (e:React.ChangeEvent<HTMLSelectElement>) => setStatus(e.target.value as Status);
-  const onChangeContributors =  (e:React.ChangeEvent<HTMLInputElement>) => setContributors(e.target.value);
-  const onChangeReviewers =  (e:React.ChangeEvent<HTMLInputElement>) => setReviewers(e.target.value);
+  useEffect(() => {
+    if (data === undefined) return;
+    setDocumentData(data);
+  }, [data]);
+  const updateDocumentState = <K extends keyof Document>(key: K, val: Document[K]) => {
+    setDocumentData(prev => prev === undefined ? prev : ({ ...prev, [key]: val }))
+  }
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("title", e.target.value);
+  const onChangeContents = (e: React.ChangeEvent<HTMLTextAreaElement>) => updateDocumentState("contents", e.target.value);
+  const onChangeStatus = (e: React.ChangeEvent<HTMLSelectElement>) => updateDocumentState("status", e.target.value as Status);
+  const onChangeContributors = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("contributors", e.target.value.split(','));
+  const onChangeReviewers = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("reviewers", e.target.value.split(','));
   const onClickSave = async () => {
+    if (uid == undefined) return;
+    if (documentData == undefined) return;
+
     try {
-      await updateDocument({
-          uid: uid,
-          documentData: {
-              id:urlParams?.id ?? "",
-              title: title,
-              contents: contents,
-              status: status,
-              owner_id: uid,
-              contributors: contributors.split(','),
-              reviewers: reviewers.split(','),
-              url_privilege: 'private',
-          }
-      });
+      await updateDocument({ uid, documentData });
       navigate(`/`);
     } catch (e) {
-        alert(`エラー: ${e?.toString()}`)
+      alert(`エラー: ${e?.toString()}`)
     }
   }
-
   return (
     <>
-    <div>
-      <div className="document-title-div">
-        <input className="document-title" type="text" value={title} onChange={onChangeTitle} ></input>
-        <button type="button" onClick={() => onClickSave()}>Save</button>
-      </div>
-      <div className="document-parameter-div">
-        <span>
-            <select name="status" value={status} onChange={onChangeStatus}>
+      <div>
+        <div className="document-title-div">
+          <input className="document-title" type="text" value={documentData?.title} onChange={onChangeTitle} ></input>
+          <button type="button" onClick={() => onClickSave()}>Save</button>
+        </div>
+        <div className="document-parameter-div">
+          <span>
+            <select name="status" value={documentData?.status} onChange={onChangeStatus}>
               <option value="draft">draft</option>
               <option value="reviewed">reviewed</option>
               <option value="final">final</option>
               <option value="obsolete">obsolete</option>
             </select>
-            <input className="authors" type="text" value={contributors} onChange={onChangeContributors}></input>
-            <input className="reviewers" type="text" value={reviewers} onChange={onChangeReviewers}></input>
-        </span>
-        <span>
+            <input className="authors" type="text" value={documentData?.contributors} onChange={onChangeContributors}></input>
+            <input className="reviewers" type="text" value={documentData?.reviewers} onChange={onChangeReviewers}></input>
+          </span>
+          <span>
             <span className="updated">
-              Updated {document?.updated_at.toDate().toLocaleString()}
+              Updated {documentData?.updated_at.toDate().toLocaleString()}
             </span>
-        </span>
-      </div>
-      <div className="document-contents-div">
-        <textarea
-          className="document-contents"
-          value={contents}
-          onChange={onChangeContents}
-          placeholder="Enter Markdown here"
-        />
-        <div className="document-markdown">
-          <Markdown>{contents}</Markdown>
+          </span>
+        </div>
+        <div className="document-contents-div">
+          <textarea
+            className="document-contents"
+            value={documentData?.contents}
+            onChange={onChangeContents}
+            placeholder="Enter Markdown here"
+          />
+          <div className="document-markdown">
+            <Markdown>{documentData?.contents}</Markdown>
+          </div>
         </div>
       </div>
-    </div>
-   </>
+    </>
   )
 }
 
