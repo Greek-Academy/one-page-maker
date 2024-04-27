37983093ac935cd681797784bb8676d97ddbeca8
Takashi Tanaka <tako.t.t.1046@gmail.com>
2024-04-27 16:11:54 +0000

    :sparkles: implement sign up and password reset page.

--- one-pager-maker/src/Router.tsx
+++ one-pager-maker/src/Router.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Edit from "./pages/Edit";
 import List from "./pages/List";
-import Login from "./pages/Login";
 
 export const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<List/>}/>
             <Route path="/edit/:id" element={<Edit/>}/>
-            <Route path="/login" element={<Login/>}/>
         </Routes>
     );
 }
