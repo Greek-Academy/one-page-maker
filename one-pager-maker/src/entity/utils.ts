eef3d80bf1009ffe8521bbbdbf07857919f6a3a2
Harineko0 <harineko0927@gmail.com>
2024-05-02 22:19:44 +0900

    Added documentRepository, documentService.

--- one-pager-maker/src/entity/utils.ts
+++ one-pager-maker/src/entity/utils.ts
@@ -1,4 +1,12 @@
-import {Timestamp, FieldValue} from "firebase/firestore";
+import {FieldValue, Timestamp} from "firebase/firestore";
+
+export type ForCreate<T> = Omit<T, "id" | "created_at" | "updated_at">;
+export type ForCreateWithId<T> = Omit<T, "created_at" | "updated_at">;
+export type ForUpdate<T extends { id: unknown }> =
+    Partial<Omit<T, "created_at" | "updated_at">>
+    & {
+    id: Pick<T, "id">["id"]
+};
 
 /**
  * 入力されたオブジェクト型のフィールドのうち, Timestamp型のフィールドのみをFieldValueに変換します.
