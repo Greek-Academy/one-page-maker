250c9f5665bf2761dea8dbf0ba28028e234eb6c8
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-05-03 17:03:04 +0900

    command test

--- one-pager-maker/src/pages/Edit.tsx
+++ one-pager-maker/src/pages/Edit.tsx
@@ -1,10 +1,11 @@
 import './Edit.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import Markdown from 'react-markdown'
 import { useAppSelector } from '../redux/hooks.ts'
 import { useUpdateDocumentMutation, useFetchDocumentQuery } from "../redux/document/documentsApi.ts";
 import { Document, Status } from "../redux/document/documentType.ts";
 import { useNavigate, useParams } from "react-router-dom";
+import { Command, CommandEmpty, CommandInput, CommandItem, CommandList } from "../../@/components/ui/command.tsx"
 
 function Edit() {
   const navigate = useNavigate();
@@ -24,7 +25,7 @@ function Edit() {
   const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("title", e.target.value);
   const onChangeContents = (e: React.ChangeEvent<HTMLTextAreaElement>) => updateDocumentState("contents", e.target.value);
   const onChangeStatus = (e: React.ChangeEvent<HTMLSelectElement>) => updateDocumentState("status", e.target.value as Status);
-  const onChangeContributors = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("contributors", e.target.value.split(','));
+  // const onChangeContributors = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("contributors", e.target.value.split(','));
   const onChangeReviewers = (e: React.ChangeEvent<HTMLInputElement>) => updateDocumentState("reviewers", e.target.value.split(','));
   const onClickSave = async () => {
     if (uid == undefined) return;
@@ -38,6 +39,31 @@ function Edit() {
     }
   }
   
+  const inputRef = useRef<HTMLInputElement>(null); //focus周りの挙動を制御するために使用
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState<string>()
+  const [searchResults, setSearchResults] = useState<string[]>([])
+  const [inputText, setInputText] = useState('')
+  const handleKeyDown = useCallback((e: React.KeyboardEvent<HTMLDivElement>) => {
+    const input = inputRef.current
+    if (input === null) return;
+    if (e.key !== "Escape") return;
+    input.blur();
+  }, [])
+  const inputStrings = ["apple", "banana", "cherry", "grape"];
+  function findMatchingStrings(inputArray: string[], targetCharacter: string): string[] {
+    const matchingStrings: string[] = [];
+    for (const str of inputArray) {
+        if (str.includes(targetCharacter)) {
+            matchingStrings.push(str);
+        }
+    }
+    return matchingStrings;
+  }  
+  useEffect(() => {
+    setSearchResults(findMatchingStrings(inputStrings, inputText))
+  }, [inputText]);
+
   return (
     <>
       <div>
@@ -53,7 +79,7 @@ function Edit() {
               <option value="final">final</option>
               <option value="obsolete">obsolete</option>
             </select>
-            <input className="authors" type="text" value={documentData?.contributors} onChange={onChangeContributors}></input>
+            {/* <input className="authors" type="text" value={documentData?.contributors} onChange={onChangeContributors}></input> */}
             <input className="reviewers" type="text" value={documentData?.reviewers} onChange={onChangeReviewers}></input>
           </span>
           <span>
@@ -73,6 +99,44 @@ function Edit() {
             <Markdown className='markdown'>{documentData?.contents}</Markdown>
           </div>
         </div>
+      </div>
+
+      <div>
+        <Command shouldFilter={false} onKeyDown={handleKeyDown} value={selected}>
+          <CommandInput value={inputText} ref={inputRef} placeholder='search' onValueChange={(text) => {
+            setInputText(text)
+            // 再編集時には選択済み項目をクリア
+            if (selected) {
+              setSelected(undefined)
+            }
+          }}
+            onBlur={() => setOpen(false)}
+            onFocus={() => {
+              setOpen(true)
+              if (selected) {
+                inputRef.current?.select() // フォーカス時に選択済み項目がある場合、全選択する
+              }
+            }}
+          />
+          <div className="relative mt-2">
+            {!selected && open && (
+              <CommandList className="absolute left-0 top-0 w-full rounded bg-background shadow-md">
+                <CommandEmpty className="text-muted-foreground px-4 py-2">ヒットなし</CommandEmpty>
+                {searchResults?.map(v => (
+                  <CommandItem
+                    className="flex items-center gap-2"
+                    onSelect={() => {
+                      setSelected(v)
+                      setInputText(v)
+                    }}
+                    value={v} key={v}>
+                    {v}
+                  </CommandItem>
+                ))}
+              </CommandList>
+            )}
+          </div>
+        </Command>
       </div>
     </>
   )
--- one-pager-maker/tailwind.config.js
+++ one-pager-maker/tailwind.config.js
@@ -1,24 +1,77 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  mode: 'jit',
-  important: true,
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {
-      aspectRatio: {
-        '3/4': '3 / 4',
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
-      colors:{
-        'google-white': '#FFFFFF',
-        'google-nature': '#F2F2F2',
-        'google-black': '#1F1F1F',
-        'github-black': '#030303',
-        'github-glay': '#444',
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
