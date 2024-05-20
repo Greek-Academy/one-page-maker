ae062850f725692c4d60d93b98d6a3c4f0e3e68a
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-05-21 00:01:06 +0900

    Change Nav

--- one-pager-maker/src/App.tsx
+++ one-pager-maker/src/App.tsx
@@ -5,7 +5,7 @@ import {auth} from './firebase.ts';
 import {login, logout} from './redux/user/userSlice';
 import {BrowserRouter, Link} from 'react-router-dom';
 import {Router} from './Router';
-import {Button} from '@mui/material';
+import {FiFileText} from "react-icons/fi";
 import {signOut} from 'firebase/auth';
 import {selectUser} from './redux/user/selector.ts';
 
@@ -36,18 +36,29 @@ function App() {
             <BrowserRouter>
                 {user ? (
                     <>
-                        <nav>
-                            <ul id="nav">
-                                <li>
-                                    <Link to="/">一覧</Link>
+                        <nav className="relative px-4 py-4 flex justify-between items-center bg-white">
+                            <Link className="text-3xl font-bold leading-none" to="/">
+                                <FiFileText className='inline' />
+                                <a className="p-1">One Pager Maker</a>
+                            </Link>
+                            <div className="lg:hidden">
+                                <button className="navbar-burger flex items-center text-blue-600 p-3">
+                                    <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <title>Mobile menu</title>
+                                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+                                <li><Link className="text-sm text-gray-400 hover:text-gray-500" to="/">Home</Link></li>
+                                <li className="text-gray-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                                    </svg>
                                 </li>
-                                <li>
-                                    <Link to="/edit">編集</Link>
-                                </li>
-                                <li>
-                                    <Button onClick={() => { signOut(auth) }}>ログアウト</Button>
-                                </li>
+                                <li><Link className="text-sm text-gray-400 hover:text-gray-500" to="/">about</Link></li>
                             </ul>
+                            <a className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" onClick={() => {signOut(auth)}}>SignOut</a>
                         </nav>
                     </>
                 ):(null)}
