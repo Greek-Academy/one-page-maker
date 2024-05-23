5efb04c43d803bc7a7ff7a82b3122a9dd6debe8c
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-05-23 23:29:47 +0900

    Review Repair And Add User

--- one-pager-maker/src/Header.tsx
+++ one-pager-maker/src/Header.tsx
@@ -4,7 +4,14 @@ import {Link} from 'react-router-dom';
 import {useAppSelector} from './redux/hooks';
 import {selectUser} from './redux/user/selector.ts';
 import {Button} from '@mui/material';
+import {FaRegUserCircle} from "react-icons/fa";
 import OnePagerMakerIcon from "./assets/OnePagerMaker.png"
+
+const NavLinkItem = ({ title }:{ title: string }) => {
+    return (
+        <Link className="text-sm text-gray-400 hover:text-gray-500" to="/">{title}</Link>        
+    );
+}
 
 function Header() {
     const user = useAppSelector(selectUser);
@@ -12,18 +19,30 @@ function Header() {
     return (
         user ? (
         <header>
-            <nav className="relative px-4 py-4 flex justify-between items-center bg-white">
-                <Link className="text-3xl font-bold leading-none" to="/">
-                    <img src={OnePagerMakerIcon} alt="onePagerMakerIcon" className="inline" />
-                    <Button type="button" className="p-1">One Pager Maker</Button>
-                </Link>
-                <ul className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 flex mx-auto items-center w-auto space-x-6">
-                    <li><Link className="text-sm text-gray-400 hover:text-gray-500" to="/">Home</Link></li>
-                    <li>|</li>
-                    <li><Link className="text-sm text-gray-400 hover:text-gray-500" to="/">About</Link></li>
-                </ul>
-                <Button  type="button" className="py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" onClick={() => {handleSignOut()}}>Sign Out</Button>                
-            </nav>
+            <div className="relative px-4 py-4 flex justify-between items-center bg-white">
+                <div>
+                    <Link className="text-3xl font-bold leading-none" to="/">
+                        <img src={OnePagerMakerIcon} alt="onePagerMakerIcon" className="inline" />
+                        <Button type="button" className="p-1">One Pager Maker</Button>
+                    </Link>
+                </div>
+                <div>
+                    <nav>
+                        <ul className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 flex mx-auto items-center w-auto space-x-6">
+                            <li><NavLinkItem title="Home"/></li>
+                            <li>|</li>
+                            <li><NavLinkItem title="About"/></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div>
+                    <Link className="leading-none px-4" to="/">
+                        <FaRegUserCircle className='inline mx-1' />
+                        {user.displayName}
+                    </Link>
+                    <Button type="button" className="py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" onClick={() => {handleSignOut()}}>Sign Out</Button>                
+                </div>
+            </div>
         </header>) : (null)
     );    
 }
