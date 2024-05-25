8e0edb5e530e0f588e35af1ef5d05ea2cf382de9
harineko0 <harineko0927@gmail.com>
2024-05-25 14:39:06 +0900

    Added ErrorContainer.tsx

---
+++ one-pager-maker/src/stories/ErrorContainer.stories.tsx
@@ -0,0 +1,23 @@
+import {Meta, StoryObj} from "@storybook/react";
+import ErrorContainer from "@/stories/ErrorContainer.tsx";
+
+const meta = {
+    title: 'ErrorContainer',
+    component: ErrorContainer,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+        layout: 'fullscreen',
+    },
+} satisfies Meta<typeof ErrorContainer>;
+
+type Story = StoryObj<typeof meta>;
+
+export const showError: Story = {
+    args: {
+        children: "Unknown Error Happened"
+    },
+};
+
+export default meta;
