a26a7b2c7459d23fa1c2fb189fd9f104aa3b9679
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-08-21 14:49:07 +0000

    chore: Refactor Router component and Entry page

--- one-pager-maker/src/Router.tsx
+++ one-pager-maker/src/Router.tsx
@@ -1,50 +1,50 @@
-import { Route, Routes } from 'react-router-dom';
-import Edit from './pages/Edit';
-import List from './pages/List';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Entry from './pages/Entry';
-import ResetPassword from './pages/ResetPassword';
-import { RouteAuthGuard } from './RouteAuthGuard';
-import SetId from '@/pages/SetId.tsx';
+import { Route, Routes } from "react-router-dom";
+import Edit from "./pages/Edit";
+import List from "./pages/List";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Entry from "./pages/Entry";
+import ResetPassword from "./pages/ResetPassword";
+import { RouteAuthGuard } from "./RouteAuthGuard";
+import SetId from "@/pages/SetId.tsx";
 
 export const Router = () => {
-    return (
-        <Routes>
-            <Route
-                path="/"
-                element={
-                    <RouteAuthGuard
-                        component={<List />}
-                        redirect="/login"
-                        status={'user-data-created'}
-                    />
-                }
-            />
-            <Route
-                path="/edit/:uid/:documentId"
-                element={
-                    <RouteAuthGuard
-                        component={<Edit />}
-                        redirect="/login"
-                        status={'user-data-created'}
-                    />
-                }
-            />
-            <Route path="/entry/:uid/:documentId" element={<Entry />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route
-                path="set-id"
-                element={
-                    <RouteAuthGuard
-                        component={<SetId />}
-                        redirect={'/login'}
-                        status={'authenticated'}
-                    />
-                }
-            />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <RouteAuthGuard
+            component={<List />}
+            redirect="/login"
+            status={"user-data-created"}
+          />
+        }
+      />
+      <Route
+        path="/edit/:uid/:documentId"
+        element={
+          <RouteAuthGuard
+            component={<Edit />}
+            redirect="/login"
+            status={"user-data-created"}
+          />
+        }
+      />
+      <Route path="/entry/:uid/:documentId" element={<Entry />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route
+        path="set-id"
+        element={
+          <RouteAuthGuard
+            component={<SetId />}
+            redirect={"/login"}
+            status={"authenticated"}
+          />
+        }
+      />
+    </Routes>
+  );
 };
