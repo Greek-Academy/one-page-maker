813e50fae70a9e8133e58250e80f97ae0c0a3057
Bin Matsui <binnmtiisprogrammer@gmail.com>
2024-08-25 00:40:01 +0900

    merge

--- one-pager-maker/src/pages/Edit.tsx
+++ one-pager-maker/src/pages/Edit.tsx
@@ -1,10 +1,5 @@
 import "./Edit.css";
 import { useEffect, useState } from "react";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
-import type { SyntaxHighlighterProps } from "react-syntax-highlighter";
 import { Document, Status } from "../entity/documentType.ts";
 import { Link, useParams } from "react-router-dom";
 import { UserSelectMenu } from "../stories/UserItem.tsx";
@@ -195,38 +190,9 @@ function Edit() {
             placeholder="Enter Markdown here"
           />
           <div className="w-1/2 overflow-visible overflow-scroll overflow-x-hidden border p-1">
-            <Markdown
-              className="markdown"
-              remarkPlugins={[remarkGfm]}
-              components={{
-                p: ({ children }) => (
-                  <p className="whitespace-pre-wrap">{children}</p>
-                ),
-                code({ className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || "");
-                  return !("inline" in props) && match ? (
-                    <SyntaxHighlighter
-                      style={vscDarkPlus}
-                      language={match[1]}
-                      PreTag="div"
-                      className="lang rounded-md text-sm"
-                      {...(props as SyntaxHighlighterProps)}
-                    >
-                      {String(children).replace(/\n$/, "")}
-                    </SyntaxHighlighter>
-                  ) : (
-                    <code
-                      {...props}
-                      className={`${className} rounded bg-gray-100 px-1 py-0.5 font-mono text-sm text-red-600`}
-                    >
-                      {children}
-                    </code>
-                  );
-                }
-              }}
-            >
-              {documentData?.contents || ""}
-            </Markdown>
+            <MarkdownRenderer
+              contents={documentData?.contents || ""}
+            ></MarkdownRenderer>
           </div>
         </div>
       </div>
